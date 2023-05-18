e68cd0d700aa5ce01c1c4f9b18f3a70643741da3
suxrobiddinovpy <123290791+suxrobiddinovpy@users.noreply.github.com>
2023-05-18 09:27:04 +0300

    Create Home.ino

---
+++ Home.ino
@@ -0,0 +1,16 @@
+void setup() {
+Serial.begin(9600);
+}
+void loop() {
+Serial.println("    *");delay(500);
+Serial.println("   * *");delay(500);
+Serial.println("  *   *");delay(500);
+Serial.println(" *     *");delay(500);
+Serial.println(" *******");delay(500);
+Serial.println(" *     *");delay(500);
+Serial.println(" * *** *");delay(500);
+Serial.println(" * * * *");delay(500);
+Serial.println(" * *** *");delay(500);
+Serial.println(" *******");
+Serial.end();
+}
